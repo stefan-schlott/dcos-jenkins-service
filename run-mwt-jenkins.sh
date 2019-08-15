b3c7603e2cd6ef706ca293d13efabd43c0cbb842
Kaiwalya Joshi <kjoshi@mesosphere.com>
2019-08-15 03:31:27 -0400

    Update run script

--- run-mwt-jenkins.sh
+++ run-mwt-jenkins.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-set -x
-#PYTEST_ARGS="\
-  #--mom=mom-4 \
-  #--jobs=5 \
-  #--work-duration=600 \
-  #--scenario=sleep \
-  #--min=0 \
-  #--max=5 \
-  #--batch-size=5 \
-  #--cpu-quota=900 \
-#"\
-#./test.sh -m scale jenkins
 
 #Cleanup scripts if needed
 #cls; DCOS_LOGIN_USERNAME="dcos-dev" DCOS_LOGIN_PASSWORD="dogfoodinit" PYTEST_ARGS="--service-ids=jenkins1,jenkins7" ./test.sh -m scalecleanup jenkins
@@ -21,21 +9,28 @@ mkdir -p $LOG_DIR
 
 LOGFILE=$LOG_DIR/jenkins_log_`date +%Y%M%d_%H%M%S`.log
 
+DCOS_LOGIN_USERNAME="bootstrapuser"
+DCOS_LOGIN_PASSWORD="deleteme" 
+
 #Ensure we're running with the correct Jenkins version.
 #dcos package repo add --index=0 jenkins-aws "${STUB_UNIVERSE_URL}"
 
-#dcos package repo remove jenkins-aws
-#export STUB_UNIVERSE_URL='https://universe-converter.mesosphere.com/transform?url=https://infinity-artifacts.s3.amazonaws.com/permanent/jenkins/assets/scale-test-jenkins-2.150.1/stub-universe-jenkins.json'
-#dcos package repo add --index=0 jenkins-aws $STUB_UNIVERSE_URL
-
 dcos package repo remove jenkins-mwt
-export STUB_UNIVERSE_URL='https://universe-converter.mesosphere.com/transform?url=https://infinity-artifacts.s3.amazonaws.com/permanent/jenkins/assets/jenkins-mwt-suppress-1/stub-universe-jenkins.json'
+export STUB_UNIVERSE_URL='https://universe-converter.mesosphere.com/transform?url=https://infinity-artifacts.s3.amazonaws.com/permanent/jenkins/assets/jenkins-mwt19-suppress-ucr-overlayfix/stub-universe-jenkins.json'
 dcos package repo add --index=0 jenkins-mwt $STUB_UNIVERSE_URL
 
 
-#cls; DCOS_LOGIN_USERNAME="bootstrapuser" DCOS_LOGIN_PASSWORD="deleteme" PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=30 --scenario=sleep --min=0 --max=1 --batch-size=1" ./test.sh -m scale jenkins |& tee -a $LOGFILE
-cls; DCOS_LOGIN_USERNAME="bootstrapuser" DCOS_LOGIN_PASSWORD="deleteme" PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=30 --scenario=sleep --min=1 --max=2 --batch-size=1" ./test.sh -m scale jenkins |& tee -a $LOGFILE
-#cls; DCOS_LOGIN_USERNAME="bootstrapuser" DCOS_LOGIN_PASSWORD="deleteme" PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=30 --scenario=sleep --min=1 --max=15 --batch-size=14" ./test.sh -m scale jenkins |& tee -a $LOGFILE
-#cls; DCOS_LOGIN_USERNAME="bootstrapuser" DCOS_LOGIN_PASSWORD="deleteme" PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=30 --scenario=sleep --min=15 --max=16 --batch-size=1" ./test.sh -m scale jenkins |& tee -a $LOGFILE
-#cls; DCOS_LOGIN_USERNAME="bootstrapuser" DCOS_LOGIN_PASSWORD="deleteme" PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=30 --scenario=sleep --min=1 --max=2 --batch-size=1" ./test.sh -m scale jenkins |& tee -a $LOGFILE
-#cls; DCOS_LOGIN_USERNAME="bootstrapuser" DCOS_LOGIN_PASSWORD="deleteme" PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=30 --scenario=sleep --min=2 --max=3 --batch-size=1" ./test.sh -m scale jenkins |& tee -a $LOGFILE
+WORK_DURATION=600
+SINGLE_USE=True
+CREATE_FRAMEWORK=True
+CREATE_JOBS=False
+
+
+cls; 
+
+#PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=$WORK_DURATION --scenario=sleep --min=0 --max=5 --batch-size=5 --create-framework=$CREATE_FRAMEWORK --create-jobs=$CREATE_JOBS --single-use=$SINGLE_USE" ./test.sh -m scale jenkins |& tee -a $LOGFILE
+#PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=$WORK_DURATION --scenario=sleep --min=5 --max=25 --batch-size=20 --create-framework=$CREATE_FRAMEWORK --create-jobs=$CREATE_JOBS --single-use=$SINGLE_USE" ./test.sh -m scale jenkins |& tee -a $LOGFILE
+#PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=$WORK_DURATION --scenario=sleep --min=25 --max=100 --batch-size=25 --create-framework=$CREATE_FRAMEWORK --create-jobs=$CREATE_JOBS --single-use=$SINGLE_USE" ./test.sh -m scale jenkins |& tee -a $LOGFILE
+
+#PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=$WORK_DURATION --scenario=sleep --min=100 --max=200 --batch-size=50 --create-framework=$CREATE_FRAMEWORK --create-jobs=$CREATE_JOBS --single-use=$SINGLE_USE" ./test.sh -m scale jenkins |& tee -a $LOGFILE
+#PYTEST_ARGS="--mom=mom-4 --jobs=5 --work-duration=$WORK_DURATION --scenario=sleep --min=200 --max=1500 --batch-size=100 --create-framework=$CREATE_FRAMEWORK --create-jobs=$CREATE_JOBS --single-use=$SINGLE_USE" ./test.sh -m scale jenkins |& tee -a $LOGFILE
